44300ad740245c477f51caab7c2fce59470264c2
Anique <aniquedavla@gmail.com>
2019-11-15 23:09:33 -0800

    navigation base

---
+++ project/BuxuryAnique/BuxuryAnique/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BuxuryAnique
+//
+//  Created by Anique Davla on 11/13/19.
+//  Copyright © 2019 Anique. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
